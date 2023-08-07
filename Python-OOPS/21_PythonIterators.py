241882ad5fccb2daa3ffbe2aa6be9a87f180c0a2
Wajahat <wajahatali4564@gmail.com>
2023-08-07 16:41:51 +0500

    first commit

--- Python-OOPS/21_PythonIterators.py
+++ Python-OOPS/21_PythonIterators.py
@@ -30,6 +30,45 @@ for i in x:
     print(i)
 
 # Create an iterators : to create an object and class you will have to implement __iter__() and __next__() to the object.
+# All classes have a function which is called __init__().
+# which allow you to do some initialization.
+
+# Example for better understanding :
+class mynumber:
+    def __iter__(self):
+        self.a = 1
+        return self
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+myclass = mynumber()
+myiter = iter(myclass)
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+# stopiteration : to prevent the iteration to go over forever ,we use the stopiteration statement 
+
+class mynum:
+    def __iter__(self):
+        self.a = 1
+        return self
+    def __next__(self):
+        if self.a <=20:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
+myclass = mynum()
+myiter = iter(myclass)
+for i in myiter:
+    print(i)
 
 
-
+#  _____________________THE END ________________________ iterators
