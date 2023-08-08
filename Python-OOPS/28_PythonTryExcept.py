979984486e4204bb71658dd6ddc72a17fb6a28bf
Wajahat <wajahatali4564@gmail.com>
2023-08-08 20:13:17 +0500

    second commit

--- Python-OOPS/28_PythonTryExcept.py
+++ Python-OOPS/28_PythonTryExcept.py
@@ -7,5 +7,59 @@
 try:
     print(x)
 except:
-    print("Error occur")
+    print("Error occur") # profession error
 
+# if try and except is not given
+# print(X) // unprofessional error
+
+# Using many exceptions
+
+try:
+    print(x)
+except NameError:
+    print("variable of x is not defined")
+except:
+    print("something else went wrong")
+
+# else - you can use else keyword to define a block of code.
+
+try:
+    print("Hello")
+except :
+    print("something  went wrong")
+else:
+    print("Nothing went wrong")
+
+# Using Finally block 
+
+try:
+    print(X)
+except :
+    print("something else went wrong")
+finally:
+    print("the 'try' except is finished")
+
+# Real Example
+
+try:
+    f = open("demofile.txt")
+    try:
+        f.write("hello community")
+    except:
+        print("something went wrong when writing to the file")
+    finally:
+        f.close()
+except:
+    print("something went wrong when opening to the file")
+
+# Raise an exception
+'''
+x = -1 
+if x < 0:
+    raise Exception("Sorry , no number is below zero")
+    '''
+
+# raise a typeerror if x is not a n integer
+str = "ali"
+if not type(str) is int:
+    raise TypeError("Only integers are allowed")
---
+++ Python-OOPS/29_PythonFileHandling.py
@@ -0,0 +1,10 @@
+
+# File handling - open()
+
+f = open("testfile.txt")
+# or
+#f = open("demofile.txt" , "rt") # rt = read text
+ 
+
+
+
