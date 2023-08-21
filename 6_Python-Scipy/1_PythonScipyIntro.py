6387759d4bedffc99a1e41d1bfc0db3655876bda
Wajahat <wajahatali4564@gmail.com>
2023-08-21 08:22:43 +0500

    first commit

---
+++ 6_Python-Scipy/1_PythonScipyIntro.py
@@ -0,0 +1,9 @@
+
+# Scipy is a scientific computation libraray that uses numpy underneath.It stand for Scientific python and providing more utilities functions for optimization , stats and signal processing.
+
+import scipy as sp
+print(sp.__version__)
+
+
+
+#__________________BEST OF LUCK ____________________#
